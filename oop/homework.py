fde01fe0d1711c3df5f38e0d1ff5e5392607ab6b
Ivan <gr3edo@gmail.com>
2019-05-29 22:23:58 +0300

    hw finished

--- oop/homework.py
+++ oop/homework.py
@@ -52,24 +52,25 @@ class Cat:
             self._increase_saturation_level(2)
 
     def _reduce_saturation_level(self, value):
-        if (self.saturation_level - value) <= 0:
-            return 0
-        else:
-            return self.saturation_level - value
+        self.saturation_level -= value
+        if self.saturation_level <= 0:
+            self.saturation_level = 0
+        return self.saturation_level
 
     def _increase_saturation_level(self, value):
-        if (self.saturation_level + value) >= 100:
-            return 100
-        else:
-            return self.saturation_level + value
+        self.saturation_level += value
+        if self.saturation_level >= 100:
+            self.saturation_level = 100
+        return self.saturation_level
 
     def _set_average_speed(self):
         if self.age <= 7:
-            return 12
+            self.average_speed = 12
         elif 7 < self.age <= 10:
-            return 9
+            self.average_speed = 9
         elif self.age > 10:
-            return 6
+            self.average_speed = 6
+        return self.average_speed
 
     def run(self, hours):
         avg_km = self.get_average_speed() * hours
@@ -119,11 +120,12 @@ class Cheetah(Cat):
 
     def _set_average_speed(self):
         if self.age <= 5:
-            return 90
+            self.average_speed = 90
         elif 5 < self.age <= 15:
-            return 90
+            self.average_speed = 75
         elif self.age > 15:
-            return 40
+            self.average_speed = 40
+        return self.average_speed
 
 
 class Wall:
@@ -148,9 +150,8 @@ class Wall:
     def wall_square(self):
         return self.width * self.height
 
-    # TODO
     def number_of_rolls_of_wallpaper(self, roll_width_m, roll_length_m):
-        pass
+        return (self.width // roll_width_m) / (roll_length_m // self.height)
 
 
 class Roof:
@@ -343,7 +344,7 @@ class House:
         return self.__door.update_wood_price(new_wood_price)
 
     def update_metal_price(self, new_metal_price):
-        return self.__door.update_wood_price(new_metal_price)
+        return self.__door.update_metal_price(new_metal_price)
 
     def get_roof_square(self):
         return self.__roof.roof_square()
@@ -363,6 +364,5 @@ class House:
         else:
             return sum([wall.number_of_rolls_of_wallpaper(roll_width_m, roll_length_m) for wall in self.__walls])
 
-# TODO
     def get_room_square(self):
-        pass
+        return self.get_walls_square() - self.get_windows_square() - self.get_door_square()
